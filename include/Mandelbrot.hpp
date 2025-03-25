c76cbc17ad70a6f3c5120f8badf38e783958b125
TolikRogov <tolik_rogov@bk.ru>
2025-03-25 16:05:56 +0300

    white loud

---
+++ include/Mandelbrot.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <stdlib.h>
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 800;
+const unsigned int PIXELS_SIZE = WINDOW_HEIGHT * WINDOW_WIDTH * 4;
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,36 @@
-#include <stdio.h>
+#include "Mandelbrot.hpp"
 
-int main() {
+int main()
+{
+	srand((unsigned int)time(NULL));
 
-	printf("Fuck you!\n");
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Mandelbrot!");
+
+	sf::Uint8* pixels = new sf::Uint8[PIXELS_SIZE];
+
+	sf::Texture texture;
+	texture.create(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		for (size_t i = 0; i < PIXELS_SIZE; i++)
+			pixels[i] = (sf::Uint8)(rand() % 255);
+		texture.update(pixels);
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
 
 	return 0;
 }
