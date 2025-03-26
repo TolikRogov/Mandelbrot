c00e50dd15ca4ee0e0b2b0e13966788a7a5544bf
TolikRogov <tolik_rogov@bk.ru>
2025-03-26 03:56:32 +0300

    picture of mandelbrot

--- include/Mandelbrot.hpp
+++ include/Mandelbrot.hpp
@@ -2,9 +2,14 @@
 
 #include "Mandelbrot_utilities.hpp"
 
-const unsigned int WINDOW_WIDTH = 800;
-const unsigned int WINDOW_HEIGHT = 800;
-const unsigned int PIXELS_SIZE = WINDOW_HEIGHT * WINDOW_WIDTH * 4;
+typedef double mandel_t;
+
+const unsigned int WINDOW_WIDTH 	= 800;
+const unsigned int WINDOW_HEIGHT 	= 800;
+const unsigned int PIXELS_SIZE 		= WINDOW_HEIGHT * WINDOW_WIDTH * 4;
+
+const int MAXIMUM_ITERATIONS 		= 256;
+const mandel_t MAX_RADIUS 			= 100;
 
 struct SFML {
 	sf::RenderWindow window;
--- source/Mandelbrot.cpp
+++ source/Mandelbrot.cpp
@@ -11,8 +11,31 @@ MandelbrotStatusCode RunSFML(SFML* sfml) {
 				sfml->window.close();
 		}
 
-		for (size_t i = 0; i < PIXELS_SIZE; i++)
-			sfml->pixels[i] = (sf::Uint8)(rand() % 255);
+		for (unsigned int y_index = 0; y_index < WINDOW_HEIGHT; y_index++) {
+			mandel_t x_0 = -2.0f;
+			mandel_t y_0 = (mandel_t)(1.5f - ((mandel_t)y_index * 3.0f / (mandel_t)WINDOW_HEIGHT));
+			for (unsigned int x_index = 0; x_index < WINDOW_WIDTH; x_index++, x_0 += 3.0f / (mandel_t)WINDOW_WIDTH) {
+				mandel_t X = x_0,
+						 Y = y_0;
+				int n = 0;
+				for (; n < MAXIMUM_ITERATIONS; n++) {
+					mandel_t x2 = X * X,
+							 y2 = Y * Y,
+							 xy = X * Y;
+					mandel_t r2 = x2 + y2;
+					if (r2 >= MAX_RADIUS) break;
+					X = x2 - y2 + x_0;
+					Y = xy + xy + y_0;
+				}
+				if (n >= MAXIMUM_ITERATIONS)
+					continue;
+				n *= 8;
+				sfml->pixels[4 * (x_index + y_index * WINDOW_WIDTH)] 		= (sf::Uint8)(n);
+				sfml->pixels[4 * (x_index + y_index * WINDOW_WIDTH) + 1] 	= (sf::Uint8)(n);
+				sfml->pixels[4 * (x_index + y_index * WINDOW_WIDTH) + 2] 	= (sf::Uint8)(128 - n);
+				sfml->pixels[4 * (x_index + y_index * WINDOW_WIDTH) + 3] 	= 255;
+			}
+		}
 		sfml->texture.update(sfml->pixels);
 
 		sfml->window.clear();
